7febd20ae953b44b3ca14eb8a7a1e74ad8b14729
prasath.webby <prasath.github@gmail.com>
2018-06-12 02:05:01 +0530

    Third-commit

--- ngapp/src/app/app.module.ts
+++ ngapp/src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {Routes,RouterModule} from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VideoCenterComponent } from './video-center/video-center.component';
+import { VideoDetailComponent } from './video-detail/video-detail.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { HomeComponent } from './home/home.component';
+
+const routes:Routes = [
+  {
+    path:'',redirectTo:'/home',pathMatch:'full'
+  },
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'videos',component:VideoCenterComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    VideoCenterComponent,
+    VideoDetailComponent,
+    VideoListComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
